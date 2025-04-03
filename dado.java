59eeb27097b8875cf75e7715c0d4201bf19d0a41
unlucky123 <66431033+unlucky123@users.noreply.github.com>
2025-04-03 17:42:09 +0200

    Update dado.java
    creadas las clases de dado rapido, dado lento y dado normal para su correcto funcionamiento

--- dado.java
+++ dado.java
@@ -1,10 +1,32 @@
 package pinguino;
 
+import java.util.Random;
+
 public class dado {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//declaramos las varibles que vamos a usar
+	private static int resultado;
+	private static Random rand = new Random();
+//hacemos un random del 0 al 5 y le sumamos 1 para que sea del 1 al 6
+	public static void DadoNormal() {
+		resultado = 0;
+		resultado = rand.nextInt(5) + 1;
+	}
+//lo mismo para el dado rapido, pero sumamos 6	
+	public static void DadoRapido() {
+		resultado = 0;
+		resultado = rand.nextInt(5) + 6;
+	}
+//lo mismo para el dado lento, pero es del 0 al 2 y le sumamos 1 para que sea del 1 al 3
+	public static void DadoLento() {
+		resultado = 0;
+		resultado = rand.nextInt(2) + 1;
+	}
+//declaramos el geter y el seter
+	public static int getResultado() {
+		return resultado;
 	}
 
+	public static void setResultado(int resultado) {
+		dado.resultado = resultado;
+	}
 }
