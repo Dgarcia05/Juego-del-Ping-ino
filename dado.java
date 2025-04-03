c12d367a40fad5977d83a3d8129a0183b87c0eff
Dgarcia05 <danielgarciamartin6@alumnes.ilerna.com>
2025-04-03 16:02:36 +0200

    Add files via upload

---
+++ dado.java
@@ -0,0 +1,10 @@
+package pinguino;
+
+public class dado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ inventario.java
@@ -0,0 +1,5 @@
+package pinguino;
+
+public class inventario {
+
+}
